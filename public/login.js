6480088efc3d06f2e38adb8e9f1289d9317f1461
Nyituriki <igihozocolombe2003@gmail.com>
2022-05-06 10:32:40 +0200

    --- Auto-Git Commit ---

--- public/login.js
+++ public/login.js
@@ -1,28 +1,10 @@
-// const form = document.querySelector('#create-account-form');
-// const emailInput = document.querySelector('#email');
-// const passwordInput = document.querySelector('#password');
 
-// form.addEventListener('submit', (event)=>{
-//     validateForm();
 
-//     console.log(isFormValid());
-//     if(isFormValid()==true){
-//         login();
-//     }
-// });
-
-// function isFormValid(){
-//     const inputContainers = form.querySelectorAll('.input-group');
-//     let result = true;
-//     inputContainers.forEach((container)=>{
-//         if(container.classList.contains('error')){
-//             result = false;
-//         }
-//     });
-//     return result;
-// }
-
-function login() {
+form.addEventListener('submit', (event)=>{
+function login(event) {
+    event.preventDefault();
+ const email=document.getElementById('email');
+ const password=document.getElementById('password');
     fetch("/user/signin",{
         method:"POST",
         headers:{
@@ -33,62 +15,13 @@ function login() {
             password
         })
     })  
-    .then(res=>
-           res.json())
-    .catch(err=>{
-            console.log(err)
-        })
-    }
-        //    localStorage.setItem("jwt",res.token)
-        //    localStorage.setItem("user",JSON.stringify(res.user))
-
-// return res.redirect('dashboard.html');
-       
- 
-// function validateForm() {
-   
-
-//     if(emailInput.value.trim()==''){
-//         setError(emailInput, 'Provide email address');
-//     }else if(isEmailValid(emailInput.value)){
-//         setSuccess(emailInput);
-//     }else{
-//         setError(emailInput, 'Provide valid email address');
-//     }
-
-   
-//     if(passwordInput.value.trim()==''){
-//         setError(passwordInput, 'Password can not be empty');
-//     }else if(passwordInput.value.trim().length <6 || passwordInput.value.trim().length >20){
-//         setError(passwordInput, 'Password min 6 max 20 charecters');
-//     }else {
-//         setSuccess(passwordInput);
-//     }
-  
-// }
-
-// function setError(element, errorMessage) {
-//     const parent = element.parentElement;
-//     if(parent.classList.contains('success')){
-//         parent.classList.remove('success');
-//     }
-//     parent.classList.add('error');
-//     const paragraph = parent.querySelector('p');
-//     paragraph.textContent = errorMessage;
-// }
-
-// function setSuccess(element){
-//     const parent = element.parentElement;
-//     if(parent.classList.contains('error')){
-//         parent.classList.remove('error');
-//     }
-//     parent.classList.add('success');
-// }
-
-// function isEmailValid(email){
-//     const reg =/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-//     return reg.test(email);
-// }
-
-
+    .then((res) => {
+            console.log("here is the data",res);
+    })
+    .then(data => {
+        localStorage.setItem("token", data.token)
+        logResponse("loginResponse", `localStorage set with token value: ${data.token}`)
+    })
+    .catch(console.error)
+}
+})
