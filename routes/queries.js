5dd92b58d43daa380be2a1baca78e93481ed6a0d
Nyituriki <igihozocolombe2003@gmail.com>
2022-05-09 19:39:32 +0200

    --- Auto-Git Commit ---

--- routes/queries.js
+++ routes/queries.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 const QueriesController=require("../controllers/queries")
 const requireLogin = require('../middleware/requireLogin')
-
+const isAdmin=require("../middleware/isAdmin");
 
 router.post("/queries",QueriesController.createQuery)
-router.get('/queries',requireLogin,QueriesController.getAllQueries)
+router.get('/queries',requireLogin,isAdmin,QueriesController.getAllQueries)
 
 module.exports=router
 
