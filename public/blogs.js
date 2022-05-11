6ea468a4d6406d11cf88ae8c56f899fd477e704b
Nyituriki <igihozocolombe2003@gmail.com>
2022-05-11 21:39:13 +0200

    --- Auto-Git Commit ---

--- public/blogs.js
+++ public/blogs.js
@@ -1,8 +1,9 @@
 
-async function loginUser(event){
-    document.getElementById("loginError").style.display = "none";
+function createBlog(event){
+  // document.getElementById("loginError").style.display = "none";
   event.preventDefault();
-
+  // console.log()
+  // alert(localStorage.getItem("token"))
     let title = document.getElementById("title").value;
     let body = document.getElementById("body").value;
     let status = document.getElementById("status").value;
@@ -11,24 +12,22 @@ async function loginUser(event){
     fetch("/article", {
                   method: 'POST',
                   headers: {
-                    Authentication: 'Bearer Token'
+                    'Authorization': localStorage.getItem('token')
                   },
-                  body: JSON.stringify({
+                      body: JSON.stringify({
                       title: title,
                       body: body,
                       body:body,
                       status:status,
                       image:image
                   })
-              }).then((res) => res.json())
+              }).then((res) => res)
+              .then((data) =>{
+                console.log(data)})
               .catch(err => console.log(err))
-              if(result.error){
-               return document.getElementById("loginError").style.display = "flex";
-              }else{
-                document.getElementById("loginError").style.display = "none";
-                localStorage.getItem("token");
-                  return window.location = "./dashboard.html"; 
-              }
+              
+                 
+              
           
            
   }
