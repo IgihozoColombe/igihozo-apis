986a1503a60215e0309bdcf61d5a5312ddbec8af
Nyituriki <igihozocolombe2003@gmail.com>
2022-05-12 12:03:02 +0200

    --- Auto-Git Commit ---

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -1,24 +1,21 @@
-const jwt = require("jsonwebtoken")
-
-module.exports = function (req, res, next){
-    const token = req.header('Authorization')
-    if(!token){
-        return res.status(400).send("Access Denied! You need to login first")
-    }
-
-    try{
-        const TokenArray = token;
-        jwt.verify(TokenArray,"e-portfolio-2022-api-tkn",(err, decoded)=>{
-            if(decoded){
-            req.user = decoded?.userId;
-            return next()
-            }
-            if(err){
-                return res.status(400).send("Invalid Token")
-            }
-        })
-    }
-    catch(ex){      
-       return res.status(400).send("Invalid Token")
-    }
-}
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const JWT_SECRET = require("../key");
+const User = mongoose.model("User");
+module.exports = (req, res, next) => {
+	const token = req.header('Authorization')
+	console.log(token)
+	if (!token) {
+		return res.status(401).json({ error: "You must be logged In" });
+	}
+	jwt.verify(token, JWT_SECRET, (err, payload) => {
+		if (err) {
+			return res.status(401).json({ error: "You must be logged In" });
+		}
+		const { _id} = payload;
+		User.findById(_id).then((userdata) => {
+			req.user = userdata;
+			next();
+		});
+	});
+};
